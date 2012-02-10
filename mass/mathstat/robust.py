b564cb4e5259ca1583d65050193ffd53ef7d2143
fowlerj <fowlerj@862728ba-e6a4-4e5a-a5d1-1d484a527e37>
2012-02-10 22:06:02 +0000

    Added robust scale statistic Q as mass.mathstat.robust.Qscale() with test code.

--- mass/mathstat/robust.py
+++ mass/mathstat/robust.py
@@ -8,7 +8,7 @@ Created on Feb 9, 2012
 @author: fowlerj
 '''
 
-__all__ = ['shorth_range',]
+__all__ = ['shorth_range','high_median', 'Qscale']
 
 import numpy #, scipy.stats
 
@@ -82,3 +82,203 @@ for both the dispersion and the location of a unimodal distribution.
 
 ....
 """
+
+
+def high_median(x, weights=None, return_index=False):
+    """Compute the weighted high median of data set x with weights <weights>.
+    
+    It returns the smallest x[j] such that the sum of all weights for data
+    with x[i] <= x[j] is strictly greater than half the total weight.
+    
+    If return_index is True, then the chosen index is returned also as (highmed, index).
+    """
+    x = numpy.asarray(x)
+    sort_idx = x.argsort()  # now x[sort_idx] is sorted
+    n = len(x)
+    if weights is None:
+        weights = numpy.ones(n)
+    else:
+        weights = numpy.asarray(weights)
+    total_weight = weights.sum()
+    
+    imin, imax = 0, n  # The possible range of j will always be the half-open interval [imin,imax)
+    left_weight = right_weight = 0 # Total weight in (...,imin) and in [imax,...)
+    itrial = n/2
+    while imax-imin > 1:
+        partial_left_weight = weights[sort_idx[imin:itrial]].sum() # from [imin,itrial)
+        partial_right_weight = weights[sort_idx[itrial+1:imax]].sum()  # from (itrial,imax)
+#        trial_weight = weights[sort_idx][itrial]
+#        print "(%d, %d, %d) weights [%d, %d <%d> %d, %d]"%(imin, itrial, imax, left_weight, partial_left_weight, trial_weight, partial_right_weight, right_weight)
+        
+        if left_weight + partial_left_weight > 0.5*total_weight: # j < itrial
+            right_weight += partial_right_weight
+            imax = itrial
+        elif right_weight + partial_right_weight >= 0.5*total_weight: # j > itrial
+            left_weight += partial_left_weight
+            imin = itrial
+        else: # j == itrial
+            if return_index:
+                ri = sort_idx[itrial]
+                return x[ri], ri
+            return x[sort_idx[itrial]]
+        itrial = (imin+imax)/2
+    
+    if return_index:
+        ri = sort_idx[itrial]
+        return x[ri], ri
+    return x[sort_idx[itrial]]
+
+
+def Qscale(x, sort_inplace=False):
+    """Compute the robust estimator of scale Q of Rousseeuw and Croux using only O(n log n)
+    memory and computations.  (A naive implementation is O(n^2) in both.)
+    
+    <x>   The data set, an unsorted sequence of values.
+    <sort_inplace> Whether it is okay for the function to reorder the set <x>.
+                   If True, <x> must be a numpy.ndarray (or ValueError is raised).
+    
+    Q is defined as d_n * 2.2219 * {|xi-xj|; i<j}_k, where 
+    
+        {a}_k means the kth order-statistic of the set {a},
+        this set is that of the distances between all (n 2) possible pairs of data in {x}
+        n=# of observations in set {x},
+        k = (n choose 2)/4,
+        2.2219 makes Q consistent for sigma in normal distributions as n-->infinity,
+        and d_n is a correction factor to the 2.2219 when n is not large.
+    
+    This function does apply the correction factors to make Q consistent with sigma for a 
+    Gaussian distribution.
+    
+    Technique from C. Croux & P. Rousseeuw in Comp. Stat Vol 1 (1992) ed. Dodge & Whittaker,
+    Heidelberg: Physica-Verlag pages 411-428.  Available at ftp://ftp.win.ua.ac.be/pub/preprints/92/Timeff92.pdf
+    
+    The estimator is further studied in Rousseeuw & Croux, J Am. Stat. Assoc 88 (1993), pp 1273-1283.
+    """
+    
+    if not sort_inplace:
+        x = numpy.array(x)
+    elif not isinstance(x, numpy.ndarray):
+        raise ValueError("sort_inplace cannot be True unless the data set x is a numpy.ndarray.")
+    x.sort()
+    n = len(x)
+    if n<2:
+        raise ValueError("Data set <x> must contain at least 2 values!")
+    h = n/2 + 1
+    target_k = h*(h-1)/2 -1 # -1 so that order count can start at 0 instead of conventional 1,2,3...
+    
+    # Compute the n-dependent prefactor to make Q consistent with sigma of a Gaussian.
+    prefactor = 2.2219
+    if n <= 9:
+        prefactor *= [0, 0, 0.399, 0.994, 0.512, 0.844, 0.611, 0.857, 0.669, 0.872][n]
+    else:
+        if n%2 == 1:
+            prefactor *= n/(n+1.4)
+        else:
+            prefactor *= n/(n+3.8)
+
+    # Now down to business finding the 25%ile of |xi - xj| for i<j (or equivalently, for i != j)
+    # Imagine the upper triangle of the matrix Aij = xj - xi (upper means j>i).
+    # If the set is sorted such that xi <= x(i+1) for any i, then the upper triangle of Aij contains
+    # exactly those distances from which we need the k=n/4 order statistic.
+    
+    # For small lists, too many boundary problems arise.  Just do it the slow way:
+    if n<=5:
+        dist = numpy.hstack([x[j]-x[:j] for j in range(1,n)])
+        assert len(dist) == (n*(n-1))/2
+        dist.sort()
+        return dist[target_k] * prefactor
+    
+    
+    # Keep track of which candidates on each ROW are still in the running.
+    # These limits are of length (n-1) because the lowest row has no upper-triangle elements. 
+    # These mean that element A_ij = xj-xi is in the running if and only if  left(i) <= j <= right(i).
+    left = numpy.arange(1, n, dtype=numpy.int)
+    right = numpy.zeros(n-1, dtype=numpy.int) + (n-1)
+    row_bias = x[numpy.arange(n-1)]
+    trial_column = numpy.zeros(n-1, dtype=numpy.int)
+    
+    def choose_trial_val(left, right, x):
+        """Choose a trial val as the weighted median of the medians of the remaining candidates in
+        each row, where the weights are the number of candidates remaining in each row."""
+        
+        w = []
+        rm = []
+        ci = []
+#        print left, right, 'xxxxxx'
+        for i,(l,r) in enumerate(zip(left, right)):
+            if l>r:
+                w.append(0)
+                rm.append(0) 
+                ci.append(0)
+                continue
+            w.append(r+1-l)
+            ctr_index = (l+r)/2
+            if ctr_index >= n:
+                ctr_index = n-1
+            rm.append(x[ctr_index] - x[i])
+            ci.append(ctr_index)
+        weights = numpy.array(w)
+        row_median = numpy.array(rm)
+
+#        print "Have to choose from", row_median, weights
+        trial_val, chosen_row =  high_median(row_median, weights=weights, return_index=True)
+        chosen_col = ci[chosen_row]
+        return trial_val, chosen_row, chosen_col
+
+#    print "Data: ", x
+#    dist = numpy.array([x-x[j] for j in range(n)])
+#    dist[dist<0] = 0
+#    print dist
+
+    for _counter in range((n*(n-1))/2):
+        trial_distance, trial_i, trial_j = choose_trial_val(left, right, x)
+        per_row_value = trial_distance + row_bias
+        
+        # In each row i, find the highest index trial_column such that x[tc]-x[i] < trial_distance
+        # If such an index is out of the candidate range, then let it be left-1 or right. 
+        # We must be extremely careful in this loop, because rounding errors can make 
+        # (trial_distance + x[i]) != x[j] even when trial distance was defined as (x[j]-x[i])
+        # So instead we have the choose_trial_val tell us the (i,j) pair being considered
+        # and test for it.
+        
+        # trial_column tracks the column in each row which is the highest column strictly less than
+        # the trial distance.
+        for i, trial_val in enumerate(per_row_value):
+            if i==trial_i:
+                trial_column[i] = trial_j-1
+                continue
+            
+            ia, ib = left[i], right[i]
+            if ia >= n:
+                trial_column[i] = n-1
+                continue
+            if x[ia] >= trial_val:
+                trial_column[i] = ia-1
+                continue
+            if x[ib] <= trial_val:
+                trial_column[i] = ib
+                continue
+            while ib-ia>1:
+                ip = (ib+ia)/2
+                if x[ip] < trial_val:
+                    ia = ip
+                elif x[ip] > trial_val:
+                    ib = ip
+                else:
+                    ia = ip-1
+                    ib = ip
+                    continue
+            trial_column[i] = ia
+        candidates_below_trial_dist = trial_column.sum() - ((n-2)*(n-1))/2
+        
+#        print 'Iter %3d: %2d candidates below trial distance of %f (ij=%d,%d)'%(_counter, candidates_below_trial_dist, trial_distance, trial_i, trial_j
+#                                                                                ), trial_column, trial_column-numpy.arange(n-1)
+        if candidates_below_trial_dist == target_k:
+            return prefactor * trial_distance
+        elif candidates_below_trial_dist > target_k:
+            right = trial_column.copy()
+            right[right>=n] = n-1
+        else:
+            left = trial_column+1
+            left[trial_i] += 1
+    raise RuntimeError("Qscale tried %d distances, which is too many"%_counter)
--- mass/mathstat/test/test_robust.py
+++ mass/mathstat/test/test_robust.py
@@ -9,7 +9,7 @@ Created on Feb 9, 2012
 '''
 import unittest
 import numpy
-from mass.mathstat.robust import shorth_range #@UnresolvedImport
+from mass.mathstat.robust import shorth_range, high_median, Qscale
 
 class Test_Shorth(unittest.TestCase):
     """Test the function shorth_range, which computes the range of the shortest half"""
@@ -51,6 +51,116 @@ class Test_Shorth(unittest.TestCase):
              msg="shorth_range has not sorted a ndarray in place when requested to do so.")
 
 
+
+class Test_High_Median(unittest.TestCase):
+    """"Test the function high_median, which returns the high weighted median of a data set."""
+    
+    def testUnweighted(self):
+        """Verify high_median when the weights are implied equal to 1"""
+        x = numpy.arange(7)
+        self.assertEqual(high_median(x), 3, msg="Did not get HM([0,1...6]) = 3.")
+        x = numpy.arange(8)
+        self.assertEqual(high_median(x), 4, msg="Did not get HM([0,1...,7]) = 4.")
+        x = numpy.arange(7,-1,-1)
+        self.assertEqual(high_median(x), 4, msg="Did not get HM([7,6,...0]) = 4.")
+        x = numpy.array([4])
+        self.assertEqual(high_median(x), 4, msg="Did not get HM([4]) = 4.")
+
+    def testWeighted(self):
+        """Verify simple cases of high_median"""
+        new_order = [3,0,1,4,2]
+        def scramble(x,w): return [x[i] for i in new_order], [w[i] for i in new_order]
+        
+        x, w = [1,2,3,4,5], [3,1,1,1,3]
+        self.assertEqual(high_median(x,w), 3, msg="Failed high_median on balanced, odd-summed weights.")
+        x, w = scramble(x,w)
+        self.assertEqual(high_median(x,w), 3, msg="Failed high_median on balanced, odd-summed weights.")
+        
+        x, w = [1,2,3,4,5], [3,1,2,1,3]
+        self.assertEqual(high_median(x,w), 3, msg="Failed high_median on balanced, even-summed weights.")
+        x, w = scramble(x,w)
+        self.assertEqual(high_median(x,w), 3, msg="Failed high_median on balanced, even-summed weights.")
+        
+        x, w = [1,2,3,4,5], [3,1,1,1,1]
+        self.assertEqual(high_median(x,w), 2, msg="Failed high_median on unbalanced odd-summed weights.")
+        x, w = scramble(x,w)
+        self.assertEqual(high_median(x,w), 2, msg="Failed high_median on unbalanced odd-summed weights.")
+        
+        x, w = [1,2,3,4,5], [4,1,1,1,1]
+        self.assertEqual(high_median(x,w), 2, msg="Failed high_median on even-summed weights.")
+        x, w = scramble(x,w)
+        self.assertEqual(high_median(x,w), 2, msg="Failed high_median on even-summed weights.")
+        
+        x, w = [1,2,3,4,5], [5,1,1,1,1]
+        self.assertEqual(high_median(x,w), 1, msg="Failed high_median on answer=lowest.")
+        x, w = scramble(x,w)
+        self.assertEqual(high_median(x,w), 1, msg="Failed high_median on answer=lowest.")
+        
+        x, w = [1,2,3,4,5], [1,1,1,1,3]
+        self.assertEqual(high_median(x,w), 4, msg="Failed high_median on answer=highest-1.")
+        x, w = scramble(x,w)
+        self.assertEqual(high_median(x,w), 4, msg="Failed high_median on answer=highest-1.")
+        
+        x, w = [1,2,3,4,5], [1,1,1,1,4]
+        self.assertEqual(high_median(x,w), 5, msg="Failed high_median on answer=highest.")
+        x, w = scramble(x,w)
+        self.assertEqual(high_median(x,w), 5, msg="Failed high_median on answer=highest.")
+        
+        x, w = [1,2,3,4,5], [1,1,1,1,5]
+        self.assertEqual(high_median(x,w), 5, msg="Failed high_median on answer=highest.")
+        x, w = scramble(x,w)
+        self.assertEqual(high_median(x,w), 5, msg="Failed high_median on answer=highest.")
+
+
+class Test_Qscale(unittest.TestCase):
+    
+    def testSimple(self):
+        x = numpy.array([1,4,5,6,8], dtype=numpy.float)
+        self.assertAlmostEqual(Qscale(x), 2.0*2.2219*.844, 3)
+        
+    def testInplace(self):
+        _ = Qscale([1,2,3,4], sort_inplace=False)
+        self.assertRaises(ValueError, Qscale, [1,2,3,4], sort_inplace=True)
+
+        x = numpy.array([4,3,2,1])
+        _=Qscale(x, sort_inplace=False)
+        self.assertEquals(x[0], 4, msg="Qscale sorted data when asked not to.")
+        _=Qscale(x, sort_inplace=True)
+        self.assertEquals(x[3], 4, msg="Qscale did not sort data when asked to sort.")
+
+    def Qslow(self, x):
+        """Compute Q the simple, slow way, as O(n^2)."""
+        x=numpy.array(x)
+        x.sort()
+        n = len(x)
+
+        prefactor = 2.2219
+        if n <= 9:
+            prefactor *= [0, 0, 0.399, 0.994, 0.512, 0.844, 0.611, 0.857, 0.669, 0.872][n]
+        else:
+            if n%2 == 1:
+                prefactor *= n/(n+1.4)
+            else:
+                prefactor *= n/(n+3.8)
+        
+        dist = numpy.hstack([x[j]-x[:j] for j in range(1,n)])
+        dist.sort()
+        h = n/2+1
+        k = h*(h-1)/2-1
+#        print n, k, dist, dist[k], prefactor
+        return prefactor * dist[k]
+        
+
+    def testRandom(self):
+#        numpy.random.seed(2348)
+        for size in (3,6,9,15,20,25,30,40,45,50,75,100,200):
+#        for size in (3,6,6,6,6,6,6):
+            data = numpy.random.standard_normal(size=size)
+            qs = self.Qslow(data)
+            qf = Qscale(data)
+            self.assertIsNotNone(qs)
+            self.assertIsNotNone(qf)
+            self.assertAlmostEqual(qs, qf, 5)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
