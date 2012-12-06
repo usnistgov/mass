d55bed1d363d64ab5f0eaf8e7510a63c16951e2f
Joseph Fowler <joe.fowler@nist.gov>
2012-12-06 09:17:31 -0700

    Fixed "import mass.gui" so that it starts the Qt4 event loop by the kludge of opening a pylab figure.

--- mass/gui/__init__.py
+++ mass/gui/__init__.py
@@ -1,14 +1,20 @@
+"""
+mass.gui module
+
+Joe Fowler, NIST Boulder Labs
+
+Contains various objects to simplify life with a (Qt4) GUI.
+"""
 
 __all__ = ['load_data_dialog', 'make_cuts_dialog']
 
 
-
-# The following magic will open and close a figure, which suffices
+# The following magic will open a figure, which suffices
 # to start the Qt4 event loop and prevent crashes.
+# I was trying to close it, too, but that led to a RuntimeError.
 import pylab, matplotlib.backends
-assert 'qt4' in matplotlib.backends.backend
-pylab.figure(56)
-pylab.close(56)
+assert 'Qt4' in matplotlib.backends.backend
+pylab.figure(1)
 
 import load_data_dialog
 import make_cuts_dialog
@@ -18,3 +24,4 @@ from make_cuts_dialog import *
 
 __all__.extend(load_data_dialog.__all__)
 __all__.extend(make_cuts_dialog.__all__)
+
