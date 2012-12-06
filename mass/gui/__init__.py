dc9bc030b4957fe965f3394b66c86b3e5cfd181e
Joe Fowler <joe.fowler@nist.gov>
2012-12-05 17:46:21 -0700

    Attempt a fix in the bug whereby trying to use mass.gui before running any pylab (that is, Qt4) functions leads to a crash with "QWidget: Must construct a QApplication before a QPaintDevice Aborted" error.

--- mass/gui/__init__.py
+++ mass/gui/__init__.py
@@ -1,6 +1,15 @@
 
 __all__ = ['load_data_dialog', 'make_cuts_dialog']
 
+
+
+# The following magic will open and close a figure, which suffices
+# to start the Qt4 event loop and prevent crashes.
+import pylab, matplotlib.backends
+assert 'qt4' in matplotlib.backends.backend
+pylab.figure(56)
+pylab.close(56)
+
 import load_data_dialog
 import make_cuts_dialog
 
