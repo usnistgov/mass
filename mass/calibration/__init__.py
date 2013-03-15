b27c03b9d7162a9bc0ac81e251650fa6e1b71f75
Galen O'Neil <galen.oneil@nist.gov>
2013-03-15 16:17:07 -0600

    added general_calibration.py and added it to __init__

--- mass/calibration/__init__.py
+++ mass/calibration/__init__.py
@@ -8,9 +8,11 @@ import energy_calibration
 import fluorescence_lines
 import gaussian_lines
 import spectra
+import general_calibration
 
 __all__ = ['energy_calibration', 'fluorescence_lines', 'gaussian_lines', 'spectra']
 
+from general_calibration import *
 from energy_calibration import *
 from fluorescence_lines import *
 from gaussian_lines import *
