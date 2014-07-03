f7df3f1132c8bdd53e6380a8e07368819379af0f
Galen O'Neil <galen.oneil@nist.gov>
2014-07-03 10:29:20 -0600

    error catching for drift_correct

--- mass/core/channel_group.py
+++ mass/core/channel_group.py
@@ -1213,7 +1213,10 @@ class TESGroup(BaseChannelGroup):
 
     def drift_correct(self, forceNew=False):
         for ds in self:
+            try:
                 ds.drift_correct(forceNew)
+            except:
+                self.set_chan_bad(ds.channum, "failed drift correct")
 
     def calibrate(self, attr, line_names,name_ext="",size_related_to_energy_resolution=10, min_counts_per_cluster=20,
                   fit_range_ev=200, excl=(), plot_on_fail=False,max_num_clusters=np.inf,max_pulses_for_dbscan=1e5, forceNew=False):
