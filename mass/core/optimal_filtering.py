c4909aa4af8f3ca8ae749934c9cbeb22b84c7455
Young Il Joe <jojo978@gmail.com>
2015-09-05 18:22:37 -0600

    Some PEP 8 fixes.

--- mass/core/optimal_filtering.py
+++ mass/core/optimal_filtering.py
@@ -1,16 +1,14 @@
-'''
+"""
 Contains classes to do classic (Fourier) and time-domain optimal filtering
 
 Created on Nov 11, 2011 from code that was in channel.py
 
 @author: fowlerj
-'''
-
-__all__ = ['Filter', 'ExperimentalFilter']
+"""
 
 import numpy as np
 import scipy as sp
-import pylab as plt
+import matplotlib.pylab as plt
 
 import mass
 import mass.mathstat.toeplitz
@@ -58,7 +56,7 @@ class Filter(object):
         pre_avg = avg_signal[:n_pretrigger].mean()
         
         # If signal is negative-going, 
-        is_negative =  (avg_signal.min()-pre_avg)/(avg_signal.max()-pre_avg) < -1
+        is_negative = (avg_signal.min()-pre_avg)/(avg_signal.max()-pre_avg) < -1
         if is_negative:
             self.peak_signal = avg_signal.min() - pre_avg
         else:
@@ -82,16 +80,15 @@ class Filter(object):
         
         self.compute(fmax=fmax, f_3db=f_3db)
 
-
-    def normalize_filter(self, q): 
-        "Rescale filter <q> so that it gives unit response to self.avg_signal"
+    def normalize_filter(self, q):
+        """Rescale filter <q> so that it gives unit response to self.avg_signal"""
         if len(q) == len(self.avg_signal):
             q *= 1 / np.dot(q, self.avg_signal)
         elif self.shorten >= 2:
             conv = np.zeros(5, dtype=np.float)
             for i in range(5):
                 conv[i] = np.dot(q, self.avg_signal[i:i+len(q)])
-            x = np.arange(-2,2.1)
+            x = np.arange(-2, 2.1)
             fit = np.polyfit(x, conv, 2)
             fit_ctr = -0.5*fit[1]/fit[0]
             fit_peak = np.polyval(fit, fit_ctr)
@@ -99,9 +96,8 @@ class Filter(object):
         else:
             q *= 1.0/np.dot(q, self.avg_signal[self.shorten:-self.shorten])
 
-
     def _compute_fourier_filter(self, fmax=None, f_3db=None):
-        "Compute the Fourier-domain filter"
+        """Compute the Fourier-domain filter"""
         if self.noise_psd is None: return
         
         # Careful: let's be sure that the Fourier domain filter is done consistently in Filter and
@@ -111,18 +107,18 @@ class Filter(object):
 #        window = power_spectrum.hamming(2*(n-1-self.shorten))
         window = 1.0
 
-        if self.shorten>0:
+        if self.shorten > 0:
             sig_ft = np.fft.rfft(self.avg_signal[self.shorten:-self.shorten]*window)
         else:
             sig_ft = np.fft.rfft(self.avg_signal * window)
 
         if len(sig_ft) != n-self.shorten:
-            raise ValueError("signal real DFT and noise PSD are not the same length (%d and %d)"
-                             %(len(sig_ft), n))
+            raise ValueError("signal real DFT and noise PSD are not the same length (%d and %d)" %
+                             (len(sig_ft), n))
             
         # Careful with PSD: "shorten" it by converting into a real space autocorrelation, 
         # truncating the middle, and going back to Fourier space
-        if self.shorten>0:
+        if self.shorten > 0:
             noise_autocorr = np.fft.irfft(self.noise_psd)
             noise_autocorr = np.hstack((noise_autocorr[:n-self.shorten-1], noise_autocorr[-n+self.shorten:]))
             noise_psd = np.fft.rfft(noise_autocorr)
@@ -132,9 +128,9 @@ class Filter(object):
         
         # Band-limit
         if fmax is not None or f_3db is not None:
-            freq = np.arange(0,n-self.shorten,dtype=np.float)*0.5/((n-1)*self.sample_time)
+            freq = np.arange(0, n-self.shorten,dtype=np.float)*0.5/((n-1)*self.sample_time)
             if fmax is not None:
-                sig_ft_weighted[freq>fmax] = 0.0
+                sig_ft_weighted[freq > fmax] = 0.0
             if f_3db is not None:
                 sig_ft_weighted /= (1+(freq*1.0/f_3db)**2)
 
@@ -157,8 +153,8 @@ class Filter(object):
             ac = self.noise_autocorr[:len(self.filt_fourier)].copy()
             self.variances['fourier'] = self.bracketR(self.filt_fourier, ac)/self.peak_signal**2
             self.variances['fourierfull'] = self.bracketR(self.filt_fourierfull, ac)/self.peak_signal**2
-#        print 'Fourier filter done.  Variance: ',self.variances['fourier'], 'V/dV: ',self.variances['fourier']**(-0.5)/2.35482
-
+            # print 'Fourier filter done.  Variance: ',self.variances['fourier'],
+            # 'V/dV: ',self.variances['fourier']**(-0.5)/2.35482
 
     def compute(self, fmax=None, f_3db=None, use_toeplitz_solver=True):
         """
@@ -166,9 +162,9 @@ class Filter(object):
         again if you want to change the frequency cutoff or f_3db rolloff point.
         """
 
-        self.fmax=fmax
-        self.f_3db=f_3db
-        self.variances={}
+        self.fmax = fmax
+        self.f_3db = f_3db
+        self.variances = {}
         self.predicted_v_over_dv = {}
         self._compute_fourier_filter(fmax=fmax, f_3db=f_3db)
 
@@ -178,7 +174,7 @@ class Filter(object):
             if len(self.noise_autocorr) < n:
                 raise ValueError("Noise autocorrelation is only %d samples long, but filter requires %d"%
                                  (len(self.noise_autocorr), n))
-            if self.shorten>0:
+            if self.shorten > 0:
                 avg_signal = self.avg_signal[self.shorten:-self.shorten]
             else:
                 avg_signal = self.avg_signal
@@ -189,8 +185,9 @@ class Filter(object):
                 Rinv_sig = ts(avg_signal)
                 Rinv_1 = ts(np.ones(n))
             else:
-                if n>6000: raise ValueError("Not allowed to use generic solver for vectors longer than 6000, because it's slow-ass.")
-                R =  sp.linalg.toeplitz(noise_corr)
+                if n > 6000:
+                    raise ValueError("Not allowed to use generic solver for vectors longer than 6000, because it's slow-ass.")
+                R = sp.linalg.toeplitz(noise_corr)
                 Rinv_sig = np.linalg.solve(R, avg_signal)
                 Rinv_1 = np.linalg.solve(R, np.ones(n))
             
@@ -201,25 +198,25 @@ class Filter(object):
                 filt_length = len(self.filt_noconst)
                 sig_ft = np.fft.rfft(self.filt_noconst)
                 freq = np.fft.fftfreq(filt_length, d=self.sample_time)
-                freq = np.abs( freq[:len(sig_ft)] )
+                freq = np.abs(freq[:len(sig_ft)])
                 if fmax is not None:
-                    sig_ft[freq>fmax] = 0.0
+                    sig_ft[freq > fmax] = 0.0
                 if f_3db is not None:
                     sig_ft /= (1.+(1.0*freq/f_3db)**2)
-                self.filt_noconst = np.fft.irfft(sig_ft, n=filt_length) # n= is needed when filt_length is ODD
+                self.filt_noconst = np.fft.irfft(sig_ft, n=filt_length)  # n= is needed when filt_length is ODD
 
             self.normalize_filter(self.filt_noconst)
             self.variances['noconst'] = self.bracketR(self.filt_noconst, noise_corr) 
 
             self.filt_baseline = np.dot(avg_signal, Rinv_sig)*Rinv_1 - Rinv_sig.sum()*Rinv_sig
-            self.filt_baseline /=  self.filt_baseline.sum()
+            self.filt_baseline /= self.filt_baseline.sum()
             self.variances['baseline'] = self.bracketR(self.filt_baseline, noise_corr)
             
             try:
                 Rpretrig = sp.linalg.toeplitz(self.noise_autocorr[:self.n_pretrigger]/self.peak_signal**2)
                 self.filt_baseline_pretrig = np.linalg.solve(Rpretrig, np.ones(self.n_pretrigger))
                 self.filt_baseline_pretrig /= self.filt_baseline_pretrig.sum()
-                self.variances['baseline_pretrig'] = self.bracketR(self.filt_baseline_pretrig, Rpretrig[0,:])
+                self.variances['baseline_pretrig'] = self.bracketR(self.filt_baseline_pretrig, Rpretrig[0, :])
             except sp.linalg.LinAlgError:
                 pass
 
@@ -233,9 +230,9 @@ class Filter(object):
         (800 MB of memory)."""
         
         if len(noise) < len(q):
-            raise ValueError("Vector q (length %d) cannot be longer than the noise (length %d)"%
-                             (len(q),len(noise)))
-        n=len(q)
+            raise ValueError("Vector q (length %d) cannot be longer than the noise (length %d)" %
+                             (len(q), len(noise)))
+        n = len(q)
         r = np.zeros(2*n-1, dtype=np.float)
         r[n-1:] = noise[:n]
         r[n-1::-1] = noise[:n]
@@ -244,23 +241,23 @@ class Filter(object):
             dot += q[i]*np.dot(r[n-i-1:2*n-i-1], q)
         return dot
     
-            
     def plot(self, axes=None):
         if axes is None:
             plt.clf()
             axis1 = plt.subplot(211)
             axis2 = plt.subplot(212)
         else:
-            axis1,axis2 = axes
+            axis1, axis2 = axes
         try:
-            axis1.plot(self.filt_noconst,color='red')
-            axis2.plot(self.filt_baseline,color='purple')
-            axis2.plot(self.filt_baseline_pretrig,color='blue')
-        except AttributeError: pass
+            axis1.plot(self.filt_noconst, color='red')
+            axis2.plot(self.filt_baseline, color='purple')
+            axis2.plot(self.filt_baseline_pretrig, color='blue')
+        except AttributeError:
+            pass
         try:
-            axis1.plot(self.filt_fourier,color='gold')
-        except AttributeError: pass
-
+            axis1.plot(self.filt_fourier, color='gold')
+        except AttributeError:
+            pass
 
     def report(self, filters=None, std_energy=5898.8):
         """Report on V/dV for all filters
@@ -272,8 +269,8 @@ class Filter(object):
         """
         
         # Handle <filters> is a single string --> convert to tuple of 1 string
-        if isinstance(filters,str):
-            filters=(filters,)
+        if isinstance(filters, str):
+            filters = (filters,)
             
         # Handle default <filters> not given.
         if filters is None:
@@ -290,7 +287,6 @@ class Filter(object):
                 print("%-20s  %10.3f  %10.4e" % (f, v_dv, var))
             except KeyError:
                 print("%-20s not known" % f)
-
 
 
 class ExperimentalFilter(Filter):
@@ -336,13 +332,12 @@ class ExperimentalFilter(Filter):
         <tau>            Time constant of exponential to filter out (in milliseconds)
         """
         
-        if isinstance(tau, (int, float)): tau = [tau]
-        self.tau = tau # in milliseconds; can be a sequence of taus
+        if isinstance(tau, (int, float)):
+            tau = [tau]
+        self.tau = tau  # in milliseconds; can be a sequence of taus
         super(self.__class__, self).__init__(avg_signal, n_pretrigger, noise_psd,
                                              noise_autocorr, fmax, f_3db, sample_time, shorten)
 
-        
-    
     def compute(self, fmax=None, f_3db=None):
         """
         Compute a set of filters.  This is called once on construction, but you can call it
@@ -369,27 +364,27 @@ class ExperimentalFilter(Filter):
         # Time domain filters
         if self.noise_autocorr is not None:
             n = len(self.avg_signal) - 2*self.shorten
-            if self.shorten>0:
+            if self.shorten > 0:
                 avg_signal = self.avg_signal[self.shorten:-self.shorten]
             else:
                 avg_signal = self.avg_signal
             assert len(self.noise_autocorr) >= n
             
-            expx = np.arange(n, dtype=np.float)*self.sample_time*1e3 # in ms
+            expx = np.arange(n, dtype=np.float)*self.sample_time*1e3  # in ms
             chebyx = np.linspace(-1, 1, n)
             
-            R = self.noise_autocorr[:n]/self.peak_signal**2 # A *vector*, not a matrix
+            R = self.noise_autocorr[:n]/self.peak_signal**2  # A *vector*, not a matrix
             ts = mass.mathstat.toeplitz.ToeplitzSolver(R, symmetric=True)
             
             unit = np.ones(n)
-            exps  = [np.exp(-expx/tau) for tau in self.tau]
+            exps = [np.exp(-expx/tau) for tau in self.tau]
             cht1 = sp.special.chebyt(1)(chebyx)
             cht2 = sp.special.chebyt(2)(chebyx)
             cht3 = sp.special.chebyt(3)(chebyx)
-            deriv = avg_signal - np.roll(avg_signal,1)
+            deriv = avg_signal - np.roll(avg_signal, 1)
             deriv[0] = 0
             
-            Rinv_sig  = ts(avg_signal)
+            Rinv_sig = ts(avg_signal)
             Rinv_unit = ts(unit)
             Rinv_exps = [ts(e) for e in exps]
             Rinv_cht1 = ts(cht1)
@@ -402,12 +397,12 @@ class ExperimentalFilter(Filter):
                 filt_length = len(vector)
                 sig_ft = np.fft.rfft(vector)
                 freq = np.fft.fftfreq(filt_length, d=self.sample_time) 
-                freq = np.abs( freq[:len(sig_ft)] )
+                freq = np.abs(freq[:len(sig_ft)])
                 if fmax is not None:
-                    sig_ft[freq>fmax] = 0.0
+                    sig_ft[freq > fmax] = 0.0
                 if f_3db is not None:
                     sig_ft /= (1.+(1.0*freq/f_3db)**2)
-                vector[:] = np.fft.irfft(sig_ft, n=filt_length) # n= is needed when filt_length is ODD
+                vector[:] = np.fft.irfft(sig_ft, n=filt_length)  # n= is needed when filt_length is ODD
                  
             if fmax is not None or f_3db is not None:
                 for vector in Rinv_sig, Rinv_unit, Rinv_cht1, Rinv_cht2, Rinv_cht3, Rinv_deriv:
@@ -415,58 +410,57 @@ class ExperimentalFilter(Filter):
                 for vector in Rinv_exps:
                     band_limit(vector, fmax, f_3db)
 
-            exp_orthogs = ['exps[%d]'%i for i in range(len(self.tau))]
+            exp_orthogs = ['exps[%d]' % i for i in range(len(self.tau))]
             orthogonalities={
-                'filt_full':(),
-                'filt_noconst' :('unit',),
-                'filt_noexp'   :exp_orthogs,
-                'filt_noexpcon':['unit']+exp_orthogs,
-                'filt_noslope' :('cht1',),
-                'filt_nopoly1' :('unit', 'cht1'),
-                'filt_nopoly2' :('unit', 'cht1', 'cht2'),
-                'filt_nopoly3' :('unit', 'cht1', 'cht2', 'cht3'),
-                'filt_noderivcon':('unit','deriv'),
+                'filt_full': (),
+                'filt_noconst': ('unit',),
+                'filt_noexp': exp_orthogs,
+                'filt_noexpcon': ['unit']+exp_orthogs,
+                'filt_noslope': ('cht1',),
+                'filt_nopoly1': ('unit', 'cht1'),
+                'filt_nopoly2': ('unit', 'cht1', 'cht2'),
+                'filt_nopoly3': ('unit', 'cht1', 'cht2', 'cht3'),
+                'filt_noderivcon': ('unit', 'deriv'),
                 }
             
 #            plt.clf()
 #            plt.plot(self.filt_fourier, color='gold',label='Fourier')
 #            for shortname in ('full','noconst','noexpcon','nopoly1'):
 #            for shortname in ('full','noconst','noexp','noexpcon','noslope','nopoly1','nopoly2','nopoly3'):
-            for shortname in ('full','noexp','noconst','noexpcon','nopoly1','noderivcon'):
-                name = 'filt_%s'%shortname
+            for shortname in ('full', 'noexp', 'noconst', 'noexpcon', 'nopoly1', 'noderivcon'):
+                name = 'filt_%s' % shortname
                 orthnames = orthogonalities[name]
                 filt = Rinv_sig
                 
                 N_orth = len(orthnames) # To how many vectors are we orthgonal?
                 if N_orth > 0:
-                    u = np.vstack((Rinv_sig, [eval('Rinv_%s'%v) for v in orthnames]))
+                    u = np.vstack((Rinv_sig, [eval('Rinv_%s' % v) for v in orthnames]))
                 else:
-                    u = Rinv_sig.reshape((1,n))
-                M = np.zeros((1+N_orth,1+N_orth), dtype=np.float)
+                    u = Rinv_sig.reshape((1, n))
+                M = np.zeros((1+N_orth, 1+N_orth), dtype=np.float)
                 for i in range(1+N_orth):
-                    M[0,i] = np.dot(avg_signal, u[i,:])
-                    for j in range(1,1+N_orth):
-                        M[j,i] = np.dot(eval(orthnames[j-1]), u[i,:])
+                    M[0, i] = np.dot(avg_signal, u[i, :])
+                    for j in range(1, 1+N_orth):
+                        M[j, i] = np.dot(eval(orthnames[j-1]), u[i, :])
                 Minv = np.linalg.inv(M)
-                weights = Minv[:,0]
+                weights = Minv[:, 0]
 
                 filt = np.dot(weights, u)
-                filt = u[0,:]*weights[0]
-                for i in range(1,1+N_orth):
-                    filt += u[i,:]*weights[i]
-                
-                
+                filt = u[0, :]*weights[0]
+                for i in range(1, 1+N_orth):
+                    filt += u[i, :]*weights[i]
+
                 self.normalize_filter(filt)
                 self.__dict__[name] = filt
                 
                 print('%15s' % name),
 #                plt.plot(filt, label=name)
-                for v in (avg_signal,np.ones(n),np.exp(-expx/self.tau[0]),sp.special.chebyt(1)(chebyx),
+                for v in (avg_signal, np.ones(n), np.exp(-expx/self.tau[0]), sp.special.chebyt(1)(chebyx),
                           sp.special.chebyt(2)(chebyx)):
-                    print('%10.5f ' % np.dot(v,filt)),
+                    print('%10.5f ' % np.dot(v, filt)),
                     
                 self.variances[shortname] = self.bracketR(filt, R)
-                print('Res=%6.3f eV = %.5f' % (5898.801*np.sqrt(8*np.log(2))*self.variances[shortname]**(.5),
+                print('Res=%6.3f eV = %.5f' % (5898.801*np.sqrt(8*np.log(2))*self.variances[shortname]**.5,
                                                (self.variances[shortname]/self.variances['full'])**.5))
 #            plt.legend()
 
