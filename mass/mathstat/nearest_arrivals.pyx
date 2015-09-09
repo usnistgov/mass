8de9c13085a1e0bba70c48c65eeb6717e250058e
Young Il Joe <jojo978@gmail.com>
2015-09-09 00:14:46 -0600

    Some cython code cleanup of mass.mathstat.nearest_arrivals.nearest_arrivals

--- mass/mathstat/nearest_arrivals.pyx
+++ mass/mathstat/nearest_arrivals.pyx
@@ -9,7 +9,8 @@ cpdef nearest_arrivals(long long[:] pulse_timestamps, long long[:] external_trig
         Py_ssize_t i = 0, j = 0, t
         long long[:] delay_from_last_trigger
         long long[:] delay_until_next_trigger
-        long long a = np.iinfo(np.int64).min, b, pt
+        long long a, b, pt
+        long long max_value
 
     num_pulses = pulse_timestamps.shape[0]
     num_triggers = external_trigger_timestamps.shape[0]
@@ -20,34 +21,57 @@ cpdef nearest_arrivals(long long[:] pulse_timestamps, long long[:] external_trig
     delay_from_last_trigger = np.zeros_like(pulse_timestamps, dtype=np.int64)
     delay_until_next_trigger = np.zeros_like(pulse_timestamps, dtype=np.int64)
 
-    if num_triggers > 0:
-        b = external_trigger_timestamps[0]
-    else:
-        b = np.iinfo(np.int64).max
+    max_value = np.iinfo(np.int64).max
 
-    while True:
-        pt = pulse_timestamps[i]
-        if pt < b:
-            if b > pulse_timestamps[0]:
-                # at this point in the code a and b are values from external_trigger_timestamps that bracket pulse_timestamp[i]
-                delay_from_last_trigger[i] = pulse_timestamps[i] - a
-                delay_until_next_trigger[i] = b - pulse_timestamps[i]
+    if num_triggers > 1:
+        a = external_trigger_timestamps[0]
+        b = external_trigger_timestamps[1]
+        j = 1
+
+        # handle the case where pulses arrive before the fist external trigger
+        while True:
+            pt = pulse_timestamps[i]
+            if pt < a:
+                delay_from_last_trigger[i] = max_value
+                delay_until_next_trigger[i] = a - pt
+                i += 1
             else:
-                # handle the case where pulses arrive before the fist external trigger
-                delay_from_last_trigger[i] = np.iinfo(np.int64).max
-                delay_until_next_trigger[i] = b - pulse_timestamps[i]
-            i += 1
-            if i >= num_pulses:
                 break
-        else:
-            j += 1
-            if j >= num_triggers:
-                for t in range(i, num_pulses):
-                    # handle the case where pulses arrive after the last external trigger
-                    delay_from_last_trigger[t] = pulse_timestamps[t] - b
-                    delay_until_next_trigger[t] = np.iinfo(np.int64).max
-                break
+
+        # at this point in the code a and b are values from external_trigger_timestamps that bracket pulse_timestamp[i]
+        while True:
+            pt = pulse_timestamps[i]
+            if pt < b:
+                delay_from_last_trigger[i] = pt - a
+                delay_until_next_trigger[i] = b - pt
+                i += 1
+                if i >= num_pulses:
+                    break
             else:
-                a, b = b, external_trigger_timestamps[j]
+                j += 1
+                if j >= num_triggers:
+                    break
+                else:
+                    a, b = b, external_trigger_timestamps[j]
+
+        # handle the case where pulses arrive after the last external trigger
+        for t in range(i, num_pulses):
+            delay_from_last_trigger[t] = pulse_timestamps[t] - b
+            delay_until_next_trigger[t] = max_value
+    elif num_triggers > 0:
+        a = b = external_trigger_timestamps[0]
+
+        for i in range(num_pulses):
+            pt = pulse_timestamps[i]
+            if pt > a:
+                delay_from_last_trigger[i] = pt - a
+                delay_until_next_trigger[i] = max_value
+            else:
+                delay_from_last_trigger[i] = max_value
+                dealay_until_next_trigger = a - pt
+    else:
+        for i in range(num_pulses):
+            delay_from_last_trigger[i] = max_value
+            delay_until_next_trigger[i] = max_value
 
     return np.asarray(delay_from_last_trigger,dtype=np.int64), np.asarray(delay_until_next_trigger, dtype=np.int64)
