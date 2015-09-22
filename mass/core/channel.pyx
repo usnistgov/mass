08f832b3866700ff9603d1a5814e9cb03946d30c
Young Il Joe <jojo978@gmail.com>
2015-09-22 15:53:24 -0600

    Making cython postpeak_deriv calculations more equivalent to the ptyhon version.

--- mass/core/channel.pyx
+++ mass/core/channel.pyx
@@ -1165,9 +1165,13 @@ class MicrocalDataSet(object):
                 s2 = pulse[peak_time + 2]
                 s3 = pulse[peak_time + 3]
                 s4 = pulse[peak_time + 4]
-                t_max_deriv = t0 = t1 = f4 * s0 + f3 * s1 + f1 * s3 + f0 * s4
+                t0 = f4 * s0 + f3 * s1 + f1 * s3 + f0 * s4
+                s0, s1, s2, s3 = s1, s2, s3, s4
+                s4 = pulse[peak_time + 5]
+                t1 = f4 * s0 + f3 * s1 + f1 * s3 + f0 * s4
+                t_max_deriv = t0 if t0 < t1 else t1
 
-                for k in range(peak_time + 5, nSamples):
+                for k in range(peak_time + 6, nSamples):
                     s0, s1, s2, s3 = s1, s2, s3, s4
                     s4 = pulse[k]
                     t2 = f4 * s0 + f3 * s1 + f1 * s3 + f0 * s4
