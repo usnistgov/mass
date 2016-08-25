34c391b98c58432f6c48beb87c0b034e60a89a64
Galen O'Neil <galen.oneil@nist.gov>
2016-08-25 09:32:42 -0600

    runtests finds all test_*.py files and runs their tests

--- test/runtests.py
+++ test/runtests.py
@@ -4,16 +4,31 @@ matplotlib.use("svg") #set to common backend so will run on semphora ci with few
 
 import mass
 import sys, imp
-from os import path
+import os
+import os.path as path
+import re
 
-# add paths to folders that contain tests
-# add new folders containing test files here
-sys.path.insert(0,path.expanduser("~/mass/mass/calibration"))
-
-# import test files as modules
-# add new test filenames here
-module_names = ["test_fits", "test_calibration", "test_algorithms"]
-modules = [imp.load_module(name,*imp.find_module(name)) for name in module_names]
+# search mass and all subdirs for files matching "test_*.py"
+module_dirs = set()
+module_paths = set()
+for dirpath, dirnames, filenames in os.walk(path.expanduser("~/mass")):
+    for filename in filenames:
+        if re.match("test_.+\.py\Z",filename) and not "build" in dirpath:
+            module_dirs.add(dirpath)
+            filepath = path.join(dirpath, filename)
+            module_paths.add(filepath)
+# add path to folders containing one or more matching files to path
+for module_dir in module_dirs:
+    sys.path.insert(0,module_dir)
+# import modules from those  files
+modules = []
+for module_path in module_paths:
+    module_name = path.splitext(path.split(module_path)[-1])[0]
+    modules.append(imp.load_module(module_name,*imp.find_module(module_name)))
+# print out the modules found
+print os.path.realpath(__file__)
+print "found the following modules to test:"
+print modules
 
 # load up all tests into a suite
 suite = unittest.TestSuite()
