f7b58d3de637c40a8652161e90a9576da53b73b1
Galen O'Neil <galen.oneil@nist.gov>
2016-10-25 11:25:25 -0600

    update tests to match api change

--- src/mass/mathstat/test/test_special.py
+++ src/mass/mathstat/test/test_special.py
@@ -12,9 +12,6 @@ from mass.mathstat import special
 
 class TestVoigtWidth(unittest.TestCase):
     """Test the Voigt width approximation."""
-    
-#    def setUp(self):
-#        self.nsamp = 100
 
 
     def test_voigt_gaussian_limit(self):
@@ -26,11 +23,11 @@ class TestVoigtWidth(unittest.TestCase):
         x = numpy.hstack(([0],10**numpy.arange(-2, 2, .1)))
         for gauss_width in (.01, .1, .2, .5, 1, 2, 5, 10, 20):
             sigma = gauss_width / (8*numpy.log(2))**.5
-            v = special.voigt(x, x0=0, hwhm=0.0, sigma=sigma)
+            v = special.voigt(x, xctr=0, hwhm=0.0, sigma=sigma)
             for vi, xi in zip(v, x):
                 self.assertAlmostEqual(vi, gaussian(xi, sigma=sigma), 7)
 
-                
+
     def test_voigt_lorentzian_limit(self):
         """Verify Voigt function in Lorentzian limit for a range of x and FWHM"""
 
@@ -39,7 +36,7 @@ class TestVoigtWidth(unittest.TestCase):
 
         x = numpy.hstack(([0],10**numpy.arange(-2, 2, .1)))
         for hwhm in (.01, .1, .2, .5, 1, 2, 5, 10, 20):
-            v = special.voigt(x, x0=0, hwhm=hwhm, sigma=0.0)
+            v = special.voigt(x, xctr=0, hwhm=hwhm, sigma=0.0)
             for vi, xi in zip(v, x):
                 self.assertAlmostEqual(vi, lorentzian(xi, hwhm=hwhm), 7)
 
@@ -61,10 +58,10 @@ class TestVoigtWidth(unittest.TestCase):
         for gauss_width in (.01, .1, .2, .5, 1, 2, 5, 10, 20):
             sigma = gauss_width / (8*numpy.log(2))**.5
             vaf = special.voigt_approx_fwhm(lor_width, gauss_width)
-            vhalf = special.voigt(x=vaf*.5, x0=0, hwhm=0.5*lor_width, sigma=sigma)
-            vpeak = special.voigt(x=0., x0=0., hwhm=0.5*lor_width, sigma=sigma)
+            vhalf = special.voigt(x=vaf*.5, xctr=0, hwhm=0.5*lor_width, sigma=sigma)
+            vpeak = special.voigt(x=0., xctr=0., hwhm=0.5*lor_width, sigma=sigma)
             self.assertAlmostEqual( 0.5, vhalf/vpeak, 3)
-                                    
-    
+
+
 if __name__ == "__main__":
     unittest.main()
