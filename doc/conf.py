ea5dfaf1258fdc45a50fe9c2ccbf773e96dc63a9
ggggggggg <galenoneil@gmail.com>
2017-11-13 10:52:44 -0700

    Working sphinx build with auto api documentation of part of mass
    Mass.gui left out for causing too many errors
    mass.mathstat left out for causing a UnicodeDecodeError

--- doc/conf.py
+++ doc/conf.py
@@ -16,9 +16,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+from recommonmark.parser import CommonMarkParser
+
+sys.path.insert(0, os.path.abspath('../src/mass'))
 
 
 # -- General configuration ------------------------------------------------
@@ -43,7 +45,8 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_parsers = {'.md': CommonMarkParser}
+source_suffix = ['.rst', '.md']
 
 # The master toctree document.
 master_doc = 'index'
@@ -86,7 +89,11 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "classic"
+html_theme_options = {
+    "rightsidebar": "true",
+    "relbarbgcolor": "black"
+}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
