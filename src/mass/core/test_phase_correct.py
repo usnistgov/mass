fa3036db6a96bd3145a7e077e18057dcfd32c7f2
ggggggggg <galen.oneil@nist.gov>
2018-11-21 16:05:54 -0700

    make a plot showing how it doesn't work

--- src/mass/core/test_phase_correct.py
+++ src/mass/core/test_phase_correct.py
@@ -13,15 +13,17 @@ import mass.core.phase_correct as phase_correct
 import logging
 LOG = logging.getLogger("mass")
 
+import pylab as plt
+
 
 class TestPhaseCorrect(ut.TestCase):
     """Test various functions that handle LJH filenames."""
 
     def test_phase_correct(self):
         # make some fake data
-        energies = np.arange(3000)
+        energies = np.arange(4000)
         ph_peaks = []
-        for i,name in enumerate(["MnKAlpha","FeKAlpha","CuKAlpha"]):
+        for i,name in enumerate(["MnKAlpha","FeKAlpha","CuKAlpha","CrKAlpha"]):
             spect = mass.spectrum_classes[name]()
             energies[i*1000:(i+1)*1000]=spect.rvs(size=1000)
             ph_peaks.append(spect.nominal_peak_energy)
@@ -30,6 +32,12 @@ class TestPhaseCorrect(ut.TestCase):
         ph = energies+phase*10
         phaseCorrector = phase_correct.phase_correct(phase, ph, use=None, ph_peaks = ph_peaks)
         corrected = phaseCorrector(phase, ph)
+        plt.figure()
+        plt.plot(phase, corrected,".")
+        plt.ylim(5875,5915)
+        plt.xlabel("phase")
+        plt.ylabel("corrected pulse height")
+        plt.show()
         print(corrected-ph)
         for i in xrange(1000):
             self.assertAlmostEqual(corrected[0],corrected[i])
