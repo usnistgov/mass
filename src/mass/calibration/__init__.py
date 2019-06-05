397edc9ffb1cdcd2948e117f0912d8bb2f63c27c
ggggggggg <galen.oneil@nist.gov>
2019-06-05 12:24:35 -0600

    progress towars lmfit models

--- src/mass/calibration/__init__.py
+++ src/mass/calibration/__init__.py
@@ -6,10 +6,12 @@ from . import energy_calibration
 from . import fluorescence_lines
 from . import line_fits
 from . import algorithms
+from . import line_lmfits
 
 from .energy_calibration import *
 from .fluorescence_lines import *
 from .line_fits import *
 from .algorithms import *
+from .line_lmfits import *
 
 from . import _highly_charged_ion_lines
