d6b95ac32800fbe3d2ec44a1055a726bf7c3439c
ggggggggg <galen.oneil@nist.gov>
2019-06-17 16:47:54 -0600

    add test_phase_correct file

---
+++ src/mass/core/test_phase_correct.py
@@ -0,0 +1,104 @@
+import mass
+import numpy as np
+import unittest as ut
+import shutil
+import os
+
+import pylab as plt
+
+
+class TestPhaseCorrect(ut.TestCase):
+    def load_data(self, clear_hdf5=True):
+        name = 'src/mass/regression_test/phase_correct_test_data_4k_pulses_mass.hdf5'
+        if os.path.isfile(name):
+            os.remove(name)
+        new_src_name = 'src/mass/regression_test/phase_correct_test_data_4k_pulses_chan1.ljh'
+        data = mass.TESGroup([new_src_name], [new_src_name])
+        ds = data.channel[1]
+        return ds
+
+    def test_phase_correct_through_microcaldataset(self, plot=False):
+        np.random.seed(1231) # the final fit resolutions are quite sensitive to this, easily varying from 3 to 5 eV
+        energies = np.arange(4000)
+        ph_peaks = []
+        line_names = ["MnKAlpha","FeKAlpha","CuKAlpha","CrKAlpha"]
+        for i,name in enumerate(line_names):
+            spect = mass.spectrum_classes[name]()
+            spect.set_gauss_fwhm(3)
+            energies[i*1000:(i+1)*1000]=spect.rvs(size=1000)
+            ph_peaks.append(spect.nominal_peak_energy)
+        phase = np.linspace(-0.6,0.6,len(energies))
+        np.random.shuffle(energies)
+        np.random.shuffle(phase)
+        ph = energies+phase*10 # this pushes the resolution up to roughly 10 eV
+
+        ds = self.load_data()
+        self.assertEqual(ds.nPulses, len(energies))
+        ds.p_filt_value_dc = ph[:]
+        ds.p_filt_value = ph[:]
+        ds.p_filt_phase = phase[:]
+        ds.phase_correct(ph_peaks=ph_peaks)
+
+        if plot:
+            plt.figure()
+            plt.plot(ds.p_filt_phase, ds.p_filt_value_dc,".",label="dc")
+            plt.plot(ds.p_filt_phase, ds.p_filt_value_phc,".", label="phc")
+            plt.legend()
+
+        resolutions = []
+        for name in line_names:
+            fitter = mass.fitter_classes[name]()
+            bin_edges = np.arange(-100,100)+fitter.spect.peak_energy
+            # bin_centers = 0.5*(bin_edges[1:]+bin_edges[:-1])
+            counts, _ = np.histogram(ds.p_filt_value_phc, bin_edges)
+            params = fitter.guess_starting_params(counts, bin_edges)
+            params[fitter.param_meaning["dP_dE"]]=1
+            hold = [fitter.param_meaning["dP_dE"]]
+            if plot: plt.figure()
+            fitter.fit(counts, bin_edges,params=params, label="full", axis=plt.gca(), hold=hold, plot=plot)
+            resolutions.append(fitter.last_fit_params_dict["resolution"][0])
+        print("achieved resolutions in phase correct through microcaldataset test", resolutions)
+        self.assertLessEqual(resolutions[0], 4.3)
+        self.assertLessEqual(resolutions[1], 4.4)
+        self.assertLessEqual(resolutions[2], 5.0)
+        self.assertLessEqual(resolutions[3], 4.1)
+
+    # def test_phase_correct(self, plot=False):
+    #     np.random.seed(1231) # the final fit resolutions are quite sensitive to this, easily varying from 3 to 5 eV
+    #     energies = np.arange(4000)
+    #     ph_peaks = []
+    #     line_names = ["MnKAlpha","FeKAlpha","CuKAlpha","CrKAlpha"]
+    #     for i,name in enumerate(line_names):
+    #         spect = mass.spectrum_classes[name]()
+    #         spect.set_gauss_fwhm(3)
+    #         energies[i*1000:(i+1)*1000]=spect.rvs(size=1000)
+    #         ph_peaks.append(spect.nominal_peak_energy)
+    #     phase = np.linspace(-0.6,0.6,len(energies))
+    #     np.random.shuffle(energies)
+    #     np.random.shuffle(phase)
+    #     ph = energies+phase*10 # this pushes the resolution up to roughly 10 eV
+
+    #     phaseCorrector = mass.core.phase_correct.phase_correct(phase, ph, use=None, ph_peaks = ph_peaks)
+    #     corrected = phaseCorrector(phase, ph)
+
+    #     resolutions = []
+    #     for name in line_names:
+    #         fitter = mass.fitter_classes[name]()
+    #         bin_edges = np.arange(-100,100)+fitter.spect.peak_energy
+    #         # bin_centers = 0.5*(bin_edges[1:]+bin_edges[:-1])
+    #         counts, _ = np.histogram(corrected, bin_edges)
+    #         params = fitter.guess_starting_params(counts, bin_edges)
+    #         params[fitter.param_meaning["dP_dE"]]=1
+    #         hold = [fitter.param_meaning["dP_dE"]]
+    #         if plot: plt.figure()
+    #         fitter.fit(counts, bin_edges,params=params, label="full", axis=plt.gca(), hold=hold, plot=plot)
+    #         resolutions.append(fitter.last_fit_params_dict["resolution"][0])
+    #     print("achieved resolutions in phase correct test", resolutions)
+    #     self.assertLessEqual(resolutions[0], 4.3)
+    #     self.assertLessEqual(resolutions[1], 4.4)
+    #     self.assertLessEqual(resolutions[2], 5.0)
+    #     self.assertLessEqual(resolutions[3], 4.1)
+
+
+if __name__ == '__main__':
+    ut.main()
