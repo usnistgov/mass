731960bd0afe7e017cf4ccad4f319e1ffa2024b1
ggggggggg <ggggggggg@users.noreply.github.com>
2019-12-17 14:48:38 -0700

    add commented sketch of future tests

--- src/mass/core/test_core.py
+++ src/mass/core/test_core.py
@@ -283,7 +283,8 @@ class TestTESGroup(ut.TestCase):
         data.compute_ats_filter(shift1=False)
         data.filter_data()
         ds = data.datasets[0]
-        hdf5_filename = data.projectors_to_hdf5(replace_output=True, n_basis=4)
+        n_basis = 5
+        hdf5_filename = data.projectors_to_hdf5(replace_output=True, n_basis=n_basis)
         output_dir = tempfile.mkdtemp()
         max_channels = 100
         n_ignore_presamples = 0
@@ -291,6 +292,42 @@ class TestTESGroup(ut.TestCase):
         n_ignore_presamples, require_experiment_state=False)
         off = mass.off.off.OffFile(off_filenames[0])
         self.assertTrue(np.allclose(off["coefs"][:, 2], ds.p_filt_value[:]))
+
+        x,y=off.recordXY(0)
+
+        with h5py.File(hdf5_filename,"r") as h5:
+            projectors = h5["1/svdbasis/projectors"][()]
+            basis = h5["1/svdbasis/basis"][()]
+        self.assertEqual(projectors.shape, (ds.nSamples, n_basis))
+        self.assertEqual(basis.shape, projectors.shape[::-1])
+        mpc = np.matmul(ds.read_trace(0), projectors)
+        self.assertTrue(np.allclose(off["coefs"][0, :], mpc))
+        # import h5py
+        # import pylab as plt
+        # also need to remove matplotlib.use("svg") from runtests.py and run only this file to avoid lots of plots
+        # are the projectors orthogonal? NO :(
+        # print "projectors.T * projectors"
+        # print np.matmul(projectors.T, projectors)
+        # print "basis * basis.T"
+        # print np.matmul(basis, basis.T)
+        # print "basis*projectors"
+        # print np.matmul(basis, projectors) # should this be the identity matrix? or just very close to it?
+
+        # plt.figure()
+        # plt.plot(basis.T)
+        # plt.title("basis.T")
+        # plt.legend(["mean", "deriv", "pulse", "svd1","svd2"])
+        # plt.figure()
+        # plt.plot(y, label="from off")
+        # plt.plot(ds.read_trace(1), label="from ljh")
+        # plt.legend()
+        # plt.figure()
+        # plt.plot(projectors)
+        # plt.legend(["mean", "deriv", "pulse", "svd1","svd2"])
+        # plt.title("projectors")
+
+        # plt.show()
+        # plt.pause(20)
 
 
 
