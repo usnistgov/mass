0584f792ccb8d984443ae7c6e9d4c475c74125f8
Joseph Fowler <drjoefowler@gmail.com>
2020-01-03 10:39:30 -0700

    Add mass.common.isstr(x) to test if a string
    This unifies several slightly different ways of doing this that are supposed to be Py2 and Py3 compatible.

---
+++ src/mass/test_common.py
@@ -0,0 +1,19 @@
+import unittest as ut
+import numpy as np
+
+from common import isstr
+
+
+class TestCommon(ut.TestCase):
+    def test_isstr(self):
+        truestrings = ("text", b"bytes", u"unicode", r"rawstrin\g")
+        notstrings = ((), [], 3, 3.4, np.array([3, 5]), None, True, False, {3:4})
+
+        for s in truestrings:
+            self.assertTrue(isstr(s), msg="isstr({}) returns False, want True".format(s))
+        for n in notstrings:
+            self.assertFalse(isstr(n), msg="isstr({}) returns True, want False".format(n))
+
+
+if __name__ == '__main__':
+    ut.main()
