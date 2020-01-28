2bc6b957a78933fd984aa45a938517c321a00521
Joseph Fowler <drjoefowler@gmail.com>
2020-01-28 11:53:23 -0700

    Fix a matplotlib warning
    I cannot reproduce #179, but if I understand the warning, this should be the solution. Fixes #179.

--- conftest.py
+++ conftest.py
@@ -9,10 +9,10 @@ import matplotlib
 
 # Suppress matplotlib warnings during tests. See
 # https://stackoverflow.com/questions/55109716/c-argument-looks-like-a-single-numeric-rgb-or-rgba-sequence
-from matplotlib.axes._axes import _log as matplotlib_axes_logger
+# from matplotlib.axes._axes import _log as matplotlib_axes_logger
+# matplotlib_axes_logger.setLevel('ERROR')
 
 matplotlib.use("svg")  # set to common backend so will run on semphora ci with fewer dependencies
-matplotlib_axes_logger.setLevel('ERROR')
 warnings.filterwarnings("ignore")
 
 # Raise the logging threshold, to reduce extraneous output during tests
