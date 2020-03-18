556aae6943e7b6d08625f5e602b7cbda62b7d067
ggggggggg <ggggggggg@users.noreply.github.com>
2020-03-18 11:54:11 -0600

    correct variabble name

--- setup.py
+++ setup.py
@@ -136,5 +136,5 @@ if __name__ == "__main__":
           cmdclass={'build': QtBuilder},
           package_dir={'mass': "mass"},
           scripts=[os.path.join(BASEDIR, "bin", "ljh_truncate")],
-          install_requires=reqs
+          install_requires=requirements
           )
