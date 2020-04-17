9a55dba514d97c34e7e4253191381aa78f3900ef
ggggggggg <ggggggggg@users.noreply.github.com>
2020-04-17 10:21:12 -0600

    works towards docs deploy

--- doc/make_and_deploy_docs_bitbucket.sh
+++ doc/make_and_deploy_docs_bitbucket.sh
@@ -1,22 +1,15 @@
 #!/usr/bin/env bash
 # build the docs
-cd doc
 make clean
 make html
-cd ..
 
+git clone git@bitbucket.org:oneilg/oneilg.bitbucket.io.git
+rm -rf oneilg.bitbucket.io/mass
+mkdir oneilg.bitbucket.io/mass
+cp -r _build/html oneilg.bitbucket.io/mass
 
-# delete all in oneilg.bitbucket.io
-rm -rf ../oneilg.bitbucket.io/mass
-# copy html docs
-mkdir ../oneilg.bitbucket.io/mass
-cp -r doc/_build/html/* ../oneilg.bitbucket.io/mass
-
-# commit to master
-cd ../oneilg.bitbucket.io/mass
-git add .
+# commit to master and push
+cd oneilg.bitbucket.io
+git add mass
 git commit -m "update mass docs"
 git push origin master
-
-# return to start directory
-cd ../mass
