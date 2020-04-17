9cfc5cc8d7582a5021126d55423f6a63947cde91
ggggggggg <ggggggggg@users.noreply.github.com>
2020-04-17 10:00:17 -0600

    remove clutter from base directory

--- conftest.py
+++ conftest.py
@@ -12,9 +12,13 @@ import matplotlib
 # from matplotlib.axes._axes import _log as matplotlib_axes_logger
 # matplotlib_axes_logger.setLevel('ERROR')
 
-matplotlib.use("svg")  # set to common backend so will run on semphora ci with fewer dependencies
-warnings.filterwarnings("ignore")
+matplotlib.use("svg")  # set to common backend so will run ci with fewer dependencies
+warnings.filterwarnings("ignore") # not sure what this does
 
 # Raise the logging threshold, to reduce extraneous output during tests
 LOG = logging.getLogger("mass")
 LOG.setLevel(logging.ERROR)
+
+# ignore files in mass/demo
+# see https://docs.pytest.org/en/latest/example/pythoncollection.html
+collect_ignore = ["mass/demo"]
