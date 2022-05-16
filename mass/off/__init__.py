cddb9ce76b0a78505463ee97d06a46ca89f58324
Joe Fowler <drjoefowler@gmail.com>
2022-05-16 09:40:27 -0600

    Fix some __all__ lists in __init__.py files

--- mass/off/__init__.py
+++ mass/off/__init__.py
@@ -4,3 +4,7 @@ from .off import OffFile
 from . import channels
 from .channels import Channel, ChannelGroup, getOffFileListFromOneFile, add_group_loop
 from .util import NoCutInds, labelPeak, labelPeaks, Recipe
+
+__all__ = ["off", "OffFile", "channels", "Channel", "ChannelGroup",
+           "getOffFileListFromOneFile", "add_group_loop",
+           "NoCutInds", "labelPeak", "labelPeaks", "Recipe"]
