2082ae7d7298ac860252b67a1d9ced50a13a4ca5
ggggggggg <ggggggggg@users.noreply.github.com>
2023-09-08 10:02:54 -0600

    remove references to mass.demo

--- mass/__init__.py
+++ mass/__init__.py
@@ -3,10 +3,6 @@ Mass: a Microcalorimeter Analysis Software Suite
 
 Python tools to analyze microcalorimeter data offline.
 
-For a demonstration of some capabilities:
->>> import mass.demo
->>> print mass.demo.helptxt # and then follow the directions
-
 
 Joe Fowler, NIST Boulder Labs.  November 2010--
 """
