5e5bb9a4ea20232cf79104ef9a8435e5943ea4ed
Joe Fowler <drjoefowler@gmail.com>
2024-05-26 17:00:34 -0600

    Start using `pyproject.toml` config.
    Get dynamic versioning. Fixes #287.

--- mass/__init__.py
+++ mass/__init__.py
@@ -9,10 +9,6 @@ Joe Fowler, NIST Boulder Labs.  November 2010--
 
 # ruff: noqa: F403, F401
 
-# This is the unique source of truth about the version number (since May 26, 2023)
-# [Recommendation 1 in https://packaging.python.org/en/latest/guides/single-sourcing-package-version/]
-__version__ = "0.8.4pre1"
-
 from .core import *
 from .calibration import *
 from .mathstat import *
