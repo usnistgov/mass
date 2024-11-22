Correct errors in filter and basis scaling

mass/core/channel.py
@@ -1356,24 +1356,24 @@ class MicrocalDataSet:  # noqa: PLR0904
                         maximum_n_pulses=4000, noise_weight_basis=True, category={}):
         assert n_basis >= 3
         assert f.is_arrival_time_safe, "requires arrival-time-safe filter"
+        assert noise_weight_basis, "basis not noise weighted is not implemented"
+
         deriv_like_model = f.dt_model
         pulse_like_model = f.signal_model
+        v_dv = f.predicted_v_over_dv
         if not len(pulse_like_model) == self.nSamples:
             raise Exception(f"filter length {len(pulse_like_model)} and nSamples {self.nSamples} don't match, "
                             "you likely need to use shift1=False in compute_ats_filter")
+
         projectors1 = np.vstack([f.const_values,
                                  f.dt_values,
                                  f.values])
-        if noise_weight_basis:
-            basis1 = np.vstack([np.ones(len(pulse_like_model), dtype=float),
-                                deriv_like_model,
-                                pulse_like_model]).T
-        else:
-            def joint_norm(x):
-                """return y such that x.dot(y)==1 or at least pretty close"""
-                return x / x.dot(x)
-            # this leads to terrible results, but I'm leaving it in for now so I don't get tempted to try adding it back for testing
-            basis1 = np.vstack([joint_norm(p) for p in projectors1]).T
+        basis1 = np.vstack([
+            np.ones(len(pulse_like_model), dtype=float),
+            deriv_like_model,
+            pulse_like_model,
+        ]).T
+
         if pulses_for_svd is None:
             pulses_for_svd, _ = self.first_n_good_pulses(maximum_n_pulses, category=category)
             pulses_for_svd = pulses_for_svd.T
@@ -1383,7 +1383,6 @@ class MicrocalDataSet:  # noqa: PLR0904
         else:
             raise Exception(
                 "use autocuts when making projectors, so it can save more info about desired cuts")
-        v_dv = f.predicted_v_over_dv
         self.pulse_model = PulseModel(projectors1, basis1, n_basis, pulses_for_svd,
                                       v_dv, pretrig_rms_median, pretrig_rms_sigma, self.filename,
                                       extra_n_basis_5lag, f_5lag.values,

mass/core/optimal_filtering.py
@@ -287,7 +287,6 @@ class FilterMaker:
             filt = np.dot(Ainv, RinvM.T)
 
         band_limit(filt.T, self.sample_time, fmax, f_3db)
-        scale = np.max(avg_signal) / np.dot(filt[0], avg_signal)
 
         if cut_pre > 0 or cut_post > 0:
             nfilt = filt.shape[0]
@@ -299,8 +298,6 @@ class FilterMaker:
         filt_dt = filt[1]
         filt_baseline = filt[2]
 
-        filt_noconst *= scale
-        filt_dt *= scale
         variance = bracketR(filt_noconst, self.noise_autocorr)
         vdv = peak / (np.log(2) * 8 * variance)**0.5
         return Filter(filt_noconst, variance, vdv, filt_dt, filt_baseline, avg_signal, dt_model, 1,
@@ -317,7 +314,7 @@ class FilterMaker:
         ns = len(avg_signal)
         pre_avg = avg_signal[cut_pre:self.n_pretrigger - 1].mean()
 
-        # Find signal's peak value. This is normally peak=(max-pretrigger).
+        # Unless passed in, find the signal's peak value. This is normally peak=(max-pretrigger).
         # If signal is negative-going, however, then peak=(pretrigger-min).
         if self.peak > 0.0:
             peak_signal = self.peak
@@ -331,13 +328,16 @@ class FilterMaker:
                 peak_signal = b - pre_avg
 
         # avg_signal: normalize to have unit peak
-        avg_signal = (avg_signal - pre_avg) / peak_signal
+        avg_signal -= pre_avg
+
+        rescale = 1 / np.max(avg_signal)
+        avg_signal *= rescale
         avg_signal[:self.n_pretrigger] = 0.0
         avg_signal = avg_signal[cut_pre:ns - cut_post]
         if self.dt_model is None:
             dt_model = None
         else:
-            dt_model = self.dt_model / peak_signal
+            dt_model = self.dt_model * rescale
             dt_model = dt_model[cut_pre:ns - cut_post]
         return avg_signal, peak_signal, dt_model
 

tests/core/test_core.py
@@ -445,14 +445,13 @@ class TestTESGroup:
 
         should_be_identity = np.matmul(pulse_model.projectors, pulse_model.basis)
         wrongness = np.abs(should_be_identity - np.identity(n_basis))
-        # ideally we could set this lower, like 1e-9, but the linear algebra needs more work
+        print(f"Wrongness matrix (abs-max is {np.amax(wrongness)})")
         print(wrongness)
-        print(np.amax(wrongness))
-        assert np.amax(wrongness) < 0.16
+        assert np.amax(wrongness) < 1e-9
         pulse_model.plot()
 
-        output_dir = tmp_path_factory.mktemp("off-2")
         # test multi_ljh2off_loop with multiple ljhfiles
+        output_dir = tmp_path_factory.mktemp("off-2")
         basename, _channum = mass.ljh_util.ljh_basename_channum(ds.filename)
         N = len(off)
         prefix = os.path.split(basename)[1]