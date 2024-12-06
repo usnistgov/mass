e1f27b7b0eb1e495f754bc2e5eefb4bdb84c8717
Joe Fowler <drjoefowler@gmail.com>
2024-12-06 13:01:02 -0700

    Restore a test that documented filter-creation API
    (as requested in PR #313 review).

--- tests/mathstat/test_power_spectrum.py
+++ tests/mathstat/test_power_spectrum.py
@@ -1,5 +1,6 @@
 import pytest
 import numpy as np
+import pylab as plt
 import mass
 
 from mass.mathstat.power_spectrum import computeSpectrum
@@ -26,3 +27,37 @@ def test_values():
     expected = np.linspace(0, 0.5, 6)
     for a, b in zip(f, expected):
         assert a == pytest.approx(b)
+
+
+# test functions used in the oct23 true bq analysis
+# where the pulses did not live in ljh files, instead the pulses needed to be passed
+# explicitly as arguments to the psd and autocorr calculation
+#
+# This documents how power spectrum and noise autocorrelation can be computed,
+# and how to use them to create a `FilterMaker` and in turn a `Filter`.
+def test_orthognal_to_exponential_filter():
+    record_len = 100
+    npre = 50
+    n_noise_records = 50
+    frametime_s = 1e-5
+    avg_pulse_values = 1000 * np.arange(record_len, dtype=float)
+    # ^ we're just testing math here, this "average pulse" is easier than making an actual pulse shape
+
+    noise_traces = rng.standard_normal((record_len, n_noise_records))
+    noise_autocorr = mass.mathstat.power_spectrum.autocorrelation_broken_from_pulses(noise_traces)
+    noise_psd_calculator = mass.mathstat.power_spectrum.PowerSpectrum(record_len // 2, dt=frametime_s)
+    window = np.ones(record_len)
+    for i in range(n_noise_records):
+        noise_psd_calculator.addDataSegment(noise_traces[:, i], window=window)
+    # test that we can plot the noise spectrum
+    noise_psd_calculator.plot()
+    plt.close()
+    noise_psd = noise_psd_calculator.spectrum()
+
+    maker = mass.FilterMaker(avg_pulse_values, npre, noise_autocorr=noise_autocorr,
+                             noise_psd=noise_psd, sample_time_sec=frametime_s)
+    filter_obj = maker.compute_5lag()
+    print("predicted resolutions")
+    filter_obj.report(std_energy=1000)
+    chosen_filter = filter_obj.values
+    np.dot(chosen_filter, avg_pulse_values[2:-2])
