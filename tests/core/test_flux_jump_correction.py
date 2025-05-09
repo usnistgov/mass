945921293eb77a6c48b29331679e72f036954dbf
Joe Fowler <drjoefowler@gmail.com>
2025-05-09 11:48:42 +0200

    Merge old, disused flux jump tests into modern

--- tests/core/test_flux_jump_correction.py
+++ tests/core/test_flux_jump_correction.py
@@ -1,5 +1,5 @@
 import numpy as np
-from mass.core.analysis_algorithms import unwrap_n
+from mass.core.analysis_algorithms import unwrap_n, correct_flux_jumps
 
 import logging
 LOG = logging.getLogger("mass")
@@ -57,3 +57,59 @@ class Test_unwrap_n:
         period = max_diff * 2.01
         unwrapped = unwrap_n(data, period, n=5)
         assert np.array_equal(unwrapped, data)
+
+
+class TestOriginalAlgorithm:
+    @staticmethod
+    def make_trend_linear(sz):
+        b = rng.integers(0, 2**16 - 1)
+        m = 4 * rng.uniform() - 2
+        trend = b + m * (sz / 2.**12) * np.arange(sz)
+        return trend
+
+    @staticmethod
+    def make_trend_poly(sz, deg):
+        max_phi0 = 2
+        p = np.zeros(deg + 1)
+        p[:-1] = (2 * max_phi0 * rng.uniform(deg) - max_phi0) * 2.**12 * (1. / sz)**(np.arange(deg, 0, -1))
+        p[-1] = 2**14 + rng.integers(0, 2 * 2**14)
+        trend = np.polyval(p, np.arange(sz))
+        return trend
+
+    @staticmethod
+    def make_trend_poly_plus_sine(sz, deg):
+        max_phi0 = 2
+        p = np.zeros(deg + 1)
+        p[:-1] = (0.1 * max_phi0 * rng.uniform() * deg - 0.05 * max_phi0) * 2.**12 * (1. / sz)**(np.arange(deg, 0, -1))
+        p[-1] = 2**14 + rng.integers(0, 2 * 2**14)
+        trend = np.polyval(p, np.arange(sz))
+
+        phase = 2 * np.pi * rng.uniform()
+        amp = 0.1 * 2**12 * rng.uniform()
+        freq = 20 * rng.uniform()
+
+        trend += amp * np.cos(2 * np.pi * (1.0 * np.arange(sz) / sz) * freq + phase)
+
+        return trend
+
+    @staticmethod
+    def add_jumps(vals):
+        njumps = 30
+        for k in range(njumps):
+            start = rng.integers(1, len(vals))
+            vals[start:] += 2**12 * rng.integers(-4, 5)
+        return vals
+
+    @staticmethod
+    def verify(orig_vals, corrected):
+        assert np.all(np.abs(orig_vals - corrected) < 1e-6)
+
+    def test_algorithm(self, N=100):
+        sz = 10000
+        g = np.full(sz, True, dtype=bool)
+        for k in range(N):
+            noise = np.abs(100 * rng.standard_normal(sz))
+            vals_orig = self.make_trend_poly_plus_sine(sz, 2) + noise
+            corrupted_vals = self.add_jumps(vals_orig.copy())
+            new_vals = correct_flux_jumps(corrupted_vals, g, 2**12)
+            self.verify(vals_orig, new_vals)
